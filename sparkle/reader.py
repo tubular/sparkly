184a5b61d4b05b6f6852e23e0db4f193cc1d524f
Dmytro Popovych <drudim.ua@gmail.com>
2016-10-22 18:20:38 +0300

    Prepare testing environment for the public release

---
+++ sparkle/reader.py
@@ -0,0 +1,7 @@
+class SparkleReader(object):
+    def cassandra(self, host, keyspace, table, consistency=None, parallelism=None, options=None):
+        """Create dataframe from the cassandra table.
+         
+        
+        """
+        pass
